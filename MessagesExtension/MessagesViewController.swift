7ea1029ec0b46dcd93070402a764dacfe6ae30a7
Charles Findlay <charlesfindlay2000-coding@yahoo.com>
2017-01-01 00:23:44 -0500

    Hooked up create new event button

--- MessagesExtension/MessagesViewController.swift
+++ MessagesExtension/MessagesViewController.swift
@@ -16,6 +16,11 @@ class MessagesViewController: MSMessagesAppViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func createNewEvent(_ sender: UIButton) {
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
